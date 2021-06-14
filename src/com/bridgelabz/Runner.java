dcfa938b2eb88c7321e95c81c03d5d67c9791df0
Ananya-Krishnappa <ananyak3395@gmail.com>
2021-06-14 23:37:40 +0530

    Added:size of binary tree

--- src/com/bridgelabz/Runner.java
+++ src/com/bridgelabz/Runner.java
@@ -5,7 +5,18 @@ public class Runner {
 		MyBinaryTree<Integer> myBinaryTree = new MyBinaryTree<>();
 		myBinaryTree.add(56);
 		myBinaryTree.add(30);
+		myBinaryTree.add(22);
+		myBinaryTree.add(11);
+		myBinaryTree.add(3);
+		myBinaryTree.add(16);
+		myBinaryTree.add(40);
 		myBinaryTree.add(70);
+		myBinaryTree.add(60);
+		myBinaryTree.add(65);
+		myBinaryTree.add(63);
+		myBinaryTree.add(67);
+		myBinaryTree.add(95);
 		myBinaryTree.inorder();
+		System.out.println("Total number of tree elements " + myBinaryTree.getSize());
 	}
 }
